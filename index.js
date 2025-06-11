868b3aba7ee0ed318275040bdb18a6441ae6fc85
Jackie <gjaslivinski15@gmail.com>
2025-06-11 10:36:52 -0400

    initialized git repository

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
     if (contrastToggle) {
-    document.body.classList =+ " dark-theme"
+    document.body.classList += " dark-theme"
     }
     else {
         document.body.classList.remove("dark-theme") 
@@ -13,15 +26,15 @@ function toggleContrast() {
 
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
     loading.classList += " modal__overlay--visible";
     emailjs
         .sendForm(
             'service_u6tg75i',
             'template_j8012nx',
             event.target,
-            'user_blalLhMjwPmb7_1vu'
+            'blalLhMjwPmb7_1vu'
         )
         .then(() => {            
             loading.classList.remove("modal__overlay--visible");
